b93337525c520a55df44603928636508a2fc2ae1
raincity <icytrain@outlook.com>
2026-06-03 10:25:57 +0800

    a

--- template/gen.cpp
+++ template/gen.cpp
@@ -1,24 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-using ll=long long;
+using ll = long long;
 
-using vi=vector<int>;
+using vi = vector<int>;
 #define pb push_back
-#define all(x) begin(x),end(x)
+#define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 
-using pi=pair<int,int>;
+using pi = pair<int, int>;
 #define fi first
 #define se second
 #define mp make_pair
 
-#define E(i,a,b) for(int i=(a);i<(b);++i)
-#define F(i,a,b) E(i,a,(b)+1)
+#define E(i, a, b) for (int i = (a); i < (b); ++i)
+#define F(i, a, b) E(i, a, (b) + 1)
 
 mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
-ll rnd(ll a,ll b){return uniform_int_distribution<ll>(a,b)(rng);}
+ll rnd(ll a, ll b) { return uniform_int_distribution<ll>(a, b)(rng); }
 
-int main(){
+int main() {
     cin.tie(0)->sync_with_stdio(0);
 }
--- template/sol.cpp
+++ template/sol.cpp
@@ -1,21 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-using ll=long long;
+using ll = long long;
 
-using vi=vector<int>;
+using vi = vector<int>;
 #define pb push_back
-#define all(x) begin(x),end(x)
+#define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 
-using pi=pair<int,int>;
+using pi = pair<int, int>;
 #define fi first
 #define se second
 #define mp make_pair
 
-#define E(i,a,b) for(int i=(a);i<(b);++i)
-#define F(i,a,b) E(i,a,(b)+1)
+#define E(i, a, b) for (int i = (a); i < (b); ++i)
+#define F(i, a, b) E(i, a, (b) + 1)
 
-int main(){
+int main() {
     cin.tie(0)->sync_with_stdio(0);
 }
